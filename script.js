8b4fc69741d3a5aaed65d7d2e0f65b194000e6f4
EBAKO CORNELIUS <ebakocornelius@gmail.com>
2024-11-21 01:30:14 +0200

    added wrok

---
+++ script.js
@@ -0,0 +1,10 @@
+// console.log('hello world');
+// let display = document.getElementById('display');
+
+// let buttons = Array.from(document.getElementByIdclassName(button));
+
+// buttons.map( button => {
+//     buttonaddEventlistener('click', (e) => {
+//         console.log('clicked')
+//     )}
+// })
