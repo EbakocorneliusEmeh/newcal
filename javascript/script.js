334ecc6810aa31c45ff0466ccf5402f7db40b23c
EBAKO CORNELIUS <ebakocornelius@gmail.com>
2024-11-29 12:16:45 +0200

    added work

--- javascript/script.js
+++ javascript/script.js
@@ -1,10 +1,11 @@
 const display = document.getElementById("display");
 
-function appendToDisplay(input){
-    display.value += input;
+function appendToDisplay(input) {
+  display.value += input;
 }
-function cleaeDisplay(){
-    display.value = "";
-}function calculate(){
-    display.value = eval(display.value);
+function cleaeDisplay() {
+  display.value = "";
+}
+function calculate() {
+  display.value = eval(display.value);
 }
