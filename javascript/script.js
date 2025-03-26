0582327c905bad43e60b681d843126baa8377bd6
EBAKO CORNELIUS <ebakocornelius@gmail.com>
2025-03-26 11:16:10 +0200

    fixing work

--- javascript/script.js
+++ javascript/script.js
@@ -3,9 +3,9 @@ const display = document.getElementById("display");
 function appendToDisplay (input) {
   display.value += input;
 }
-function cleaeDisplay () {
+function clearDisplay () {
   display.value = ""
 }
 function calculate () {
-  display.value = eval(display.value);
+  display.value = eval(display.value)
 }
